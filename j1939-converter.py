5c29df4e0d16a9c705dd7835d0b14cc85e277eaf
lumbago <ecape0207@gmail.com>
2025-06-04 13:52:44 -0600

    Refactored code for readability

--- j1939-converter.py
+++ j1939-converter.py
@@ -1,22 +1,40 @@
 import argparse
 import sys
+import json
 from typing import List
 
 class J1939:
+    # Can Frame
+    can_frame: str
+    can_id: str
+    can_data: str
+
+    # Can Frame Data
+    priority: int
+    reserved: int
+    data_page: int
+    pdu_format: int
+    pdu_specific: int
+    source_address: int
+    pgn: int
+
     def __init__(self, can_frame: str):
         self.can_frame = can_frame
+
+        # Separate the ID and Data from the CAN Frame
         id_and_data = can_frame.split('#', 1)
-        
-        # Check if frame is valid
+
+        # Check and see if CAN Frame is in correct format
         if len(id_and_data) != 2:
             raise ValueError(f'ERROR: Invalid CAN Frame: {can_frame}')
         
         self.can_id, self.can_data = id_and_data
+
         
-        # Convert the CAN ID to binary
-        can_id_binary = self.__convert_id_to_binary()
-    
-        # Parse through data and store into variables
+        # Convert the CAN Id to binary for later processing
+        can_id_binary = self.__id_to_binary()
+        
+        # Extract the fields from binary ID
         self.priority = int(can_id_binary[0:3], 2)
         self.reserved = int(can_id_binary[3], 2)
         self.data_page = int(can_id_binary[4], 2)
@@ -24,39 +42,63 @@ class J1939:
         self.pdu_specific = int(can_id_binary[13:21], 2)
         self.source_address = int(can_id_binary[21:29], 2)
 
-        # Calculate the PDU based off PDU Format
-        if self.pdu_format < 240:
-            self.pgn = (self.data_page << 16) + (self.pdu_format << 8) + self.pdu_specific
+        self.pgn = self.__determine_pgn()
 
-        else:
-            self.pgn = (self.data_page << 16) + (self.pdu_format << 8)
-
-    # Description: Converts the CAN ID to binary and returns a string of that binary
+    # Description: Converts the CAN_ID to binary
     # Parameters: N\A
-    # Returns: String
-    def __convert_id_to_binary(self) -> str:
+    # Returns: str
+    def __id_to_binary(self) -> str:
         return format(int(self.can_id, 16), '029b')
     
-    # Description: Prints a summary of the parsed CAN Frame
+    # Description: Calculate the PGN
+    # Parameters: N\A
+    # Returns: int
+    def __determine_pgn(self) -> int:
+        if self.pdu_format < 240:
+            return (self.data_page << 16) + (self.pdu_format << 8) + self.pdu_specific
+        else:
+            return(self.data_page << 16) + (self.pdu_format << 8)
+
+    # Description: Prints a summary of the information gathered
     # Parameters: N\A
     # Returns: N\A
     def summary(self):
-        print(f'CAN Frame: {self.can_frame}')
+        print(f'\nCAN Frame: {self.can_frame}')
         print(f'Source Address: {self.source_address} (0x{self.source_address:X})')
         print(f'PGN: {self.pgn} (0x{self.pgn:X})')
-        print('*' * 40)
+        print("*" * 50)
+        
         print(f'Priority: {self.priority}')
         print(f'Reserved: {self.reserved}')
         print(f'Data Page: {self.data_page}')
         print(f'PDU Format: {self.pdu_format} (0x{self.pdu_format:X})')
-        print(f'PDU Specific: {self.pdu_specific} (0x{self.pdu_specific:X})')
-        print('-' * 40)
-        print()
+        print(f'PDU Specific: {self.pdu_specific} (0x{self.pdu_specific:X})\n')
 
-# Description: Parses the J1939 Log file for CAN IDs and Parses each Frame
-# Parameters: File Path to log file
-# Returns: List of J1939 Objects
-def parse_file(file_path:str) -> List[J1939]:
+# Description: Parse command line arguments using ArgParse
+# Parameters: N\A
+# Returns: parse_args object
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description = 'Parse J1939 CAN Frames and extract the PGN'
+    )
+
+    parser.add_argument(
+        '-l', '--line',
+        help='Parse a single CAN Frame (Example: 18FEF200#180194018502FFFF)'
+    )
+
+    parser.add_argument(
+        '-F', '--file',
+        help='Parse a J1939 candump formatted J1939 Log File'
+    )
+
+    return parser.parse_args()
+
+# Description: Parses a candump-style log file into J1939 Objects
+# Parameters: filepath (string)
+# Returns: List 
+def parse_log_file(file_path: str):
     frames = []
 
     # Open the file in read mode and parse each line
@@ -75,41 +117,23 @@ def parse_file(file_path:str) -> List[J1939]:
     
     return frames
 
-# DescriptionL Using argparse, create a description and add the arguments 
-def parse_args():
-    parser = argparse.ArgumentParser(
-        description='Parse J1939 CAN Frames and extract PGNs'
-    )
-    
-    parser.add_argument(
-        '-l', '--line',
-        help='Parse a single CAN Frame (e.g., 18FEE000#1BC9A8001BC9A800)'
-    )
-    
-    parser.add_argument(
-        '-F', '--file',
-        help='Parse a J1939 Log for CAN Frames (in can-utils candump format)'
-    )
-    return parser.parse_args()
-
 def main():
-    args = parse_args()
+    arguments = parse_arguments()
 
-    # Convert single CAN Frame 
-    if args.line:
+    if arguments.line:
         try:
-            frame = J1939(args.line)
+            frame = J1939(arguments.line)
             frame.summary()
 
         except ValueError as error:
             print(f'ERROR: {error}')
             sys.exit(1)
     
-    # Read a log file
-    elif args.file:
-        frames = parse_file(args.file)
+    elif arguments.file:
+        frames = parse_log_file(arguments.file)
         for frame in frames:
             frame.summary()
-    
+
+
 if __name__ == '__main__':
     main()
